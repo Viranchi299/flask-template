db933deb3269cfc2e63ec38410358d16a0aadaa2
Viranchi299 <vpatel29@gmail.com>
2021-02-19 17:18:17 -0800

    back to using blueprints

--- website/views.py
+++ website/views.py
@@ -4,7 +4,13 @@ from .models import Note
 from . import db
 import json
 
+#file stuff
+import os
+from os.path import join, dirname, realpath
+from flask import send_file, send_from_directory, safe_join, abort
+
 views = Blueprint('views', __name__)
+# views=Flask(__name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
@@ -44,6 +50,12 @@ def guide():
     return render_template("guide.html")             
 
 
+
+
+# Upload folder
+# UPLOAD_FOLDER = 'static/files'
+# app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
+
 @views.route('/submitted', methods=['POST','GET'])
 def submit_info():
     if request.method == 'POST':
@@ -62,6 +74,11 @@ def submit_info():
             #print(key,val)
             print(key, val)     
             output_vals.append(val)
+
+        # uploaded_file=request.files['file']
+        # if uploaded_file.filename !='':
+        #     file_path = os.path.join(views.config['UPLOAD_FOLDER'],uploaded_file.filename)
+        #     uploaded_file.save(file_path)            
               
         length=len(output_vals)
         print(length)             
